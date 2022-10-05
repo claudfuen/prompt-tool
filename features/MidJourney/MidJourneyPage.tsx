20d915b1436d6e54520e60820080d58c6906c158
Claudio Fuentes <imclaudfuen@gmail.com>
2022-10-05 08:04:47 -0700

    remove link to Stable Diff

--- features/MidJourney/MidJourneyPage.tsx
+++ features/MidJourney/MidJourneyPage.tsx
@@ -170,14 +170,6 @@ const MidJourneyPage = () => {
     <div>
       <MidJourneyHead title="MidJourney Prompt Tool" />
       <FacebookPixel pixelId="734778551076768" />
-      <a
-        className="bg-primary-100 text-white hover:text-white text-center p-2 flex justify-center items-center cursor-pointer hover:bg-primary-200 gap-2"
-        href="https://www.diffusion.land/"
-        target="_blank"
-      >
-        <p className="text-lg">✨ NEW: Generate Images with Stable Diffusion</p>
-        <ArrowRightIcon className="h-4 w-4" />
-      </a>
       <MidJourneyNavbar />
       <div className="py-8 p-4 sm:px-16 max-w-6xl mx-auto flex flex-col gap-4 midjourney justify-center overflow-y-auto items-center">
         <ModelSelector activeTab="midjourney" />
