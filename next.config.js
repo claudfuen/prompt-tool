30568bdaf5834f38813de10ce6a278f556aaf57b
Claudio Fuentes <imclaudfuen@gmail.com>
2022-10-31 08:49:40 -0700

    fix sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require("@sentry/nextjs");
 
 // Disabled Strict Mode, as react-beautiful-dnd library has issues with StrictMode enabled.
 const nextConfig = {
@@ -10,21 +9,6 @@ const nextConfig = {
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  include: ".",
-  org: "noonshot",
-  project: "noonshot-platform",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+export default nextConfig;
