5052f17a29010d4516fe9f06de0afd5ad8a418af
Claudio Fuentes <imclaudfuen@gmail.com>
2022-10-31 08:59:42 -0700

    export with module

--- next.config.js
+++ next.config.js
@@ -11,4 +11,4 @@ const nextConfig = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-export default nextConfig;
+module.exports = nextConfig;
